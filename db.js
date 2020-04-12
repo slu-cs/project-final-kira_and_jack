20ce22e08f6a3757e90035bd30fcb333e18c5f3e
Kira <kiramurphy13@yahoo.com>
2020-04-12 11:44:48 -0400

    db file

---
+++ db.js
@@ -0,0 +1,19 @@
+// Function for database connection.
+const mongoose = require('mongoose');
+
+module.exports = function() {
+
+  // Avoid warnings
+  mongoose.set('useNewUrlParser', true);
+  mongoose.set('useUnifiedTopology', true);
+  mongoose.set('useCreateIndex', true);
+  mongoose.set('useFindAndModify', false);
+
+  // Start connecting
+  mongoose.connect('mongodb://localhost/catalog');
+
+  // Log errors
+  mongoose.connection.on('error', function(error) {
+    console.error(error.stack);
+  });
+};
