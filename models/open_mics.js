9890f74ddcde8aa31f431936b5b9650590ec4e63
Jack Pattison <jepatt16@gmail.com>
2020-04-12 12:36:54 -0400

    open mic stuff

--- models/open_mics.js
+++ models/open_mics.js
@@ -4,19 +4,8 @@ const mongoose = require('mongoose');
 // Define the schema
 const OpenMic = new mongoose.Schema({
   date: {type: Date, required: true, match: /CS\d\d\d/},
-  title: {type: String, maxlength: 100, trim: true},
   description: {type: String, maxlength: 1000, trim: true},
-  prereqs: [{type: String, match: /CS\d\d\d/}]
-});
-
-// Clean up sections and prereqs when a course is deleted
-Course.post('findOneAndDelete', function(course) {
-  const queries = [
-    mongoose.model('Section').deleteMany({course: course.id}),
-    mongoose.model('Course').updateMany({prereqs: course.id}, {$pull: {prereqs: course.id}})
-  ];
-  Promise.all(queries).catch(error => next(error));
 });
 
 // Export the model
-module.exports = mongoose.model('Course', Course);
+module.exports = mongoose.model('OpenMic', OpenMic);
