5549d10fb4079a627d2516d43aaf22e783b158c1
Jack Pattison <jepatt16@gmail.com>
2020-04-12 18:06:01 -0400

    date formatting is now an instance method of the model

--- models/shows.js
+++ models/shows.js
@@ -7,8 +7,14 @@ const Show = new mongoose.Schema({
   time: {type: String},
   performers: {type: String, maxlength: 240, trim: true, required: true},
   description: {type: String, maxlength: 1000, trim: true},
-  link: {type: String, required: false}
+  link: {type: String, required: false},
 });
+
+Show.methods.date_string = function(){
+      const options = { weekday: 'long', month: 'long', day: 'numeric' };
+      const date_string = new Date(this.date).toLocaleDateString("en-US", options);
+      return date_string + ", " + this.time;
+  };
 
 // Export the model
 module.exports = mongoose.model('show', Show);
