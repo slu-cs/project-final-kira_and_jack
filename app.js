3a5f52b0f51c48e121abd7d0a21ef2220a0fa87b
Jack Pattison <jepatt16@gmail.com>
2020-04-12 13:20:53 -0400

    dummy home page

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(function(request, response, next) {
 
 // Redirect from the home page
 app.get('/', function(request, response) {
-  response.redirect('/courses');
+  response.render('index');
 });
 
 // Route content requests
