aecf93b1f4018679ce15f214109a704d2debcc3e
Kira <kiramurphy13@yahoo.com>
2020-04-12 15:10:43 -0400

    another error fix

--- controllers/open_mics.js
+++ controllers/open_mics.js
@@ -16,6 +16,6 @@ const OpenMic = require('../models/open_mics');
 module.exports.index = function(request, response, next) {
   const order = request.query.sort || 'name'; // Default to sort by course
   OpenMic.find().sort(order)
-    .then(openmics => response.render('open_mics/index', {openmics: openmics, order: order})
+    .then(openmics => response.render('open_mics/index', {openmics: openmics, order: order}))
     .catch(error => next(error));
 };
