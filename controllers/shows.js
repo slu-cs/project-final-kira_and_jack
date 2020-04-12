47a9fb01a8db1ff81bb5ea8e56f6ef1fecf751c6
Jack Pattison <jepatt16@gmail.com>
2020-04-12 14:54:51 -0400

    list shows

--- controllers/shows.js
+++ controllers/shows.js
@@ -1,7 +1,10 @@
-module.exports.index = function(request, response) {
-  response.send('GET /shows');
+const Shows = require('../models/shows.js')
+
+module.exports.index = function(request, response, next) {
+  Shows.distinct('date')
+    .catch(error => next(error));
 };
 
-module.exports.retrieve = function(request, response){
+module.exports.retrieve = function(request, response, next){
     response.send(`GET /shows/${request.params.date}`);
 };
