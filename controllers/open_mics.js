0a34bae89179f558ca0ebe76cd3a065f1ef8f4ec
Kira <kiramurphy13@yahoo.com>
2020-04-12 15:13:07 -0400

    test

--- controllers/open_mics.js
+++ controllers/open_mics.js
@@ -13,9 +13,7 @@ const OpenMic = require('../models/open_mics');
 //   };
 
 
-module.exports.index = function(request, response, next) {
-  const order = request.query.sort || 'name'; // Default to sort by course
-  OpenMic.find().sort(order)
-    .then(openmics => response.render('open_mics/index', {openmics: openmics, order: order}))
-    .catch(error => next(error));
-};
+module.exports.retrieve = function(request, response, next) {
+      console.log(OpenMic.findById(request.params));
+
+}
