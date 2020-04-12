8cd23d6e5c1f474b853cbe2713524966af8e8e84
Jack Pattison <jepatt16@gmail.com>
2020-04-12 16:22:51 -0400

    shows table

--- models/shows.js
+++ models/shows.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 
 // Define the schema
 const Show = new mongoose.Schema({
-  date: Date,
-  performers: {type: String, maxlength: 240, trim: true},
+  date: {type: Date, required: true},
+  performers: {type: String, maxlength: 240, trim: true, required: true},
   description: {type: String, maxlength: 1000, trim: true},
 });
 
