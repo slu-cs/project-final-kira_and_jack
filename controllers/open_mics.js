3139049cb14cb01ec0d96fccdeb9795fb2f443a6
Kira <kiramurphy13@yahoo.com>
2020-04-12 15:54:19 -0400

    sorta working open mics

--- controllers/open_mics.js
+++ controllers/open_mics.js
@@ -6,6 +6,6 @@ module.exports.index = function(request, response, next) {
   const order = request.query.sort || 'name'; // Default to sort by course
 
   OpenMic.find().sort(order)
-    .then(sections => response.render('sections/index', {names: names, order: order}))
+    .then(names => response.render('openmics/index', {names: names, order: order}))
     .catch(error => next(error));
 };
