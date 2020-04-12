334bc6b9f419305cc0444d603663d479c377d0e8
Kira <kiramurphy13@yahoo.com>
2020-04-12 14:56:58 -0400

    bug fix

--- controllers/open_mics.js
+++ controllers/open_mics.js
@@ -1,3 +1,5 @@
+const OpenMic = require('../models/open_mics');
+
 module.exports.index = function(request, response) {
     response.send('GET /open_mics');
   };
@@ -6,8 +8,6 @@ module.exports.retrieve = function(request, response){
     response.send(`GET /open_mics/${request.params.date}`);
 };
 
-
-const OpenMic = require('../models/course');
 
 // GET /courses
 module.exports.index = function(request, response, next) {
