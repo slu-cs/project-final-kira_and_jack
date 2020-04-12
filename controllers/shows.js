c4edcec38d55527611aeb0fe04577f0562cedece
Jack Pattison <jepatt16@gmail.com>
2020-04-12 14:36:25 -0400

    show routing stuff

--- controllers/shows.js
+++ controllers/shows.js
@@ -1,3 +1,7 @@
 module.exports.index = function(request, response) {
   response.send('GET /shows');
 };
+
+module.exports.retrieve = function(request, response){
+    respnse.send('GET /shows/${request.params.date}');
+};
