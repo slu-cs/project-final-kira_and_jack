902ad6e1194e12184bf8aaa384e369351cba9613
Jack Pattison <jepatt16@gmail.com>
2020-04-26 19:07:27 -0400

    admin mode (purely theoretical)

--- router.js
+++ router.js
@@ -7,6 +7,15 @@ const signups = require('./controllers/signups')
 // Create the router
 const router = express.Router();
 
+// Check for admin status
+const authorize = function(request, response, next) {
+  if (request.session.admin) {
+    next(); // Fulfill the request
+  } else {
+    response.status(401).end();
+  }
+};
+
 // handle content requests
 router.get('/shows', shows.index);
 router.get('/shows/:date', shows.retrieve);
