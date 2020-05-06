cb9e500776b433a910f9a9803fc0c37b1d691662
Kira <kiramurphy13@yahoo.com>
2020-05-06 18:01:51 -0400

    removed sign up views and controller

--- router.js
+++ router.js
@@ -2,7 +2,6 @@ const express = require('express');
 const shows = require('./controllers/shows');
 const open_mics = require('./controllers/open_mics')
 const suggestions = require('./controllers/suggestions')
-const signups = require('./controllers/signups')
 
 // Create the router
 const router = express.Router();
@@ -22,19 +21,16 @@ router.get('/shows/:id', shows.retrieve);
 router.get('/open_mics', open_mics.index);
 router.get('/open_mics/:date', open_mics.retrieve);
 router.get('/suggestions', suggestions.index);
-router.get('/signups', signups.index);
 router.get('/suggestions/thanks', suggestions.thanks)
 
 // post requests
 router.post('/suggestions', suggestions.create);
-router.post('/signups', signups.create);
 router.post('/shows', authorize, shows.create);
 router.post('/open_mics', open_mics.create);
 
 // delete requests
 router.delete('/suggestions/:id', authorize, suggestions.delete);
 router.delete('/shows/:id', authorize, shows.delete);
-router.delete('/signups/:id', authorize, signups.delete);
 router.delete('/open_mics/:id', authorize, open_mics.delete);
 
 // edit show
