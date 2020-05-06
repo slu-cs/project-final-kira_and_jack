fc8e6dedf4dd2da6a96571751e6c56ae11401393
Jack Pattison <jepatt16@gmail.com>
2020-05-06 19:22:49 -0400

    sort shows by date

--- controllers/shows.js
+++ controllers/shows.js
@@ -2,6 +2,7 @@ const Shows = require('../models/shows.js')
 
 module.exports.index = function(request, response, next) {
     Shows.find()
+        .sort('date')
         .then(shows => response.render('shows/index', {shows: shows}))
         .catch(error => next(error));
 };
@@ -23,11 +24,11 @@ module.exports.retrieve = function(request, response, next){
 // new show request
 module.exports.create = function (request, response, next) {
     Shows.create({
-            date: new Date(request.params.date + request.params.time),
-            performers: request.params.performers,
-            description: request.params.description,
-            link: request.params.link,
-            image: request.params.image
+            date: Date.parse(request.body.date + 'T' + request.body.time + ':00'),
+            performers: request.body.performers,
+            description: request.body.description,
+            link: request.body.link,
+            image: request.body.image
         })
       .then(data => response.status(201).send(data))
       .catch(error => next(error));
@@ -35,7 +36,6 @@ module.exports.create = function (request, response, next) {
 
 // edit show
 module.exports.update = function (request, response, next) {
-    console.log(request.body.date + 'T' + request.body.time + ':00')
     Shows.findByIdAndUpdate(request.params.id, {
             date: Date.parse(request.body.date + 'T' + request.body.time + ':00'),
             performers: request.body.performers,
