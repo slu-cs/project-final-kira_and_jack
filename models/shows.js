91b2fa5a6ea03a030837051241db5af5e06dc1cd
Jack Pattison <jepatt16@gmail.com>
2020-05-06 20:25:03 -0400

    date_string function for shows

--- models/shows.js
+++ models/shows.js
@@ -3,17 +3,16 @@ const mongoose = require('mongoose');
 
 // Define the schema
 const Show = new mongoose.Schema({
-  date: {type: Date}, // date objects are a hassle
+  date: {type: Date, required: true}, // date objects are a hassle
   performers: {type: String, maxlength: 240, trim: true, required: true},
-  description: {type: String, maxlength: 1000, trim: true},
+  description: {type: String, maxlength: 1000, trim: true, required: false},
   link: {type: String, required: false},
   image: {type: String, required:false}
 });
 
 Show.methods.date_string = function(){
-      const options = { weekday: 'long', month: 'long', day: 'numeric' };
-      const date_string = new Date(this.date).toLocaleDateString("en-US", options);
-      return date_string + ", " + this.time;
+     const options = {weekday: 'long', month: 'long', day: 'numeric', hour12: true, hour:'numeric', minute:'numeric'};
+     return this.date.toLocaleDateString("en-US", options);
   };
 
 // Export the model
